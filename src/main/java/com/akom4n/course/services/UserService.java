2c305992948c9a7c24e9894ca689c31befba3d31
Vinicius Silva <xdenik13@gmail.com>
2023-09-12 20:12:12 -0300

    Exception handling - findById

--- src/main/java/com/akom4n/course/services/UserService.java
+++ src/main/java/com/akom4n/course/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.akom4n.course.entities.User;
 import com.akom4n.course.repositories.UserRepository;
+import com.akom4n.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
