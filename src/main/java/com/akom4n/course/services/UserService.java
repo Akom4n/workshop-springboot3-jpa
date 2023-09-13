cde8bc991513272e09dd3a700e626512b116d4c4
Vinicius Silva <xdenik13@gmail.com>
2023-09-13 15:20:37 -0300

    Exception handling - Update

--- src/main/java/com/akom4n/course/services/UserService.java
+++ src/main/java/com/akom4n/course/services/UserService.java
@@ -11,8 +11,9 @@ import org.springframework.stereotype.Service;
 import com.akom4n.course.entities.User;
 import com.akom4n.course.repositories.UserRepository;
 import com.akom4n.course.services.exceptions.DatabaseException;
-import com.akom4n.course.services.exceptions.DatabaseException;
 import com.akom4n.course.services.exceptions.ResourceNotFoundException;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class UserService {
@@ -44,9 +45,13 @@ public class UserService {
 	}
 	
 	public User update(Long id, User obj) {
-		User entity = repository.getReferenceById(id);
-		updateData(entity, obj);
-		return repository.save(entity);
+		try {
+			User entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		}catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
